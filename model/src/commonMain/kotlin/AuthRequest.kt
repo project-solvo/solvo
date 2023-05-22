3f0ef613a9dc5c08792f43de92360625d007a5d7
Jerry Zhu <yuqian.zhu21@imperial.ac.uk>
2023-05-22 17:26:17 +0100

    changed RegisterReqeust.kt to AuthRequest

---
+++ model/src/commonMain/kotlin/AuthRequest.kt
@@ -0,0 +1,25 @@
+package org.solvo.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+class AuthRequest(
+    val username: String,
+    val hash: ByteArray,
+)
+
+@Serializable
+class AuthResponse(
+    val status: AuthStatus,
+    val token: String = "",
+)
+
+enum class AuthStatus{
+    SUCCESS,
+    INVALID_USERNAME,
+    USERNAME_TOO_LONG,
+    DUPLICATED_USERNAME,
+    USER_NOT_FOUND,
+    WRONG_PASSWORD,
+}
+
