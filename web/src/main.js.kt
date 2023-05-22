58605ae714df936f01de1eb9787f8e1cfb067851
Fengkai Liu <fl1121@ic.ac.uk>
2023-05-22 18:24:33 +0100

    Construct homepage UI and courses cards.
    Signed-off-by: Fengkai Liu <fl1121@ic.ac.uk>

--- web/src/main.js.kt
+++ web/src/main.js.kt
@@ -1,6 +1,8 @@
 package org.solvo.web
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Notifications
@@ -9,8 +11,11 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import org.jetbrains.skiko.wasm.onWasmReady
 import org.solvo.web.accounts.LoginSignUpContent
@@ -27,7 +32,7 @@ fun main() {
 @Composable
 private fun MainContent() {
     val model = remember { RegisterLoginViewModel() }
-//    HomePageContent()
+    // HomePageContent()
     LoginSignUpContent(model)
 }
 
@@ -37,36 +42,58 @@ fun HomePageContent() {
         var notifyMenu = false
         var personalMenu = false
         var settingMenu = false
-        Column {
-            TopAppBar(
-                title = {
-                    Text(text = "Solvo")
-                },
-                navigationIcon = {
-                    IconButton(onClick = {/* Go to home page */ }) {
-                        Icon(Icons.Filled.Home, null)
-                    }
-                },
-                actions = {
-                    IconButton(onClick = { notifyMenu = !notifyMenu }) {
-                        Icon(Icons.Filled.Notifications, null)
-                    }
-                    IconButton(onClick = { personalMenu = !personalMenu }) {
-                        Icon(Icons.Filled.Person, null)
-                    }
-                    IconButton(onClick = { settingMenu = !settingMenu }) {
-                        Icon(Icons.Filled.Settings, null)
-                    }
-                    DropdownMenu(
-                        expanded = notifyMenu,
-                        onDismissRequest = { notifyMenu = false },
-                        offset = DpOffset(x = 10.dp, y = (-60).dp)
-                    ) {
-                        DropdownMenuItem(
-                            text = {},
-                            onClick = {},
-                        )
-                    }
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            createTopAppBar(notifyMenu, personalMenu, settingMenu)
+            Text(
+                text = "Courses",
+                fontSize = MaterialTheme.typography.headlineLarge.fontSize,
+                modifier = Modifier.padding(25.dp),
+            )
+            val courses = mutableListOf<String>()
+            for (i in 50001 until 50012) {
+                courses.add(i.toString())
+            }
+            createCourses(courses)
+        }
+    }
+}
+
+@Composable
+private fun createTopAppBar(notifyMenu: Boolean, personalMenu: Boolean, settingMenu: Boolean) {
+    var notifyMenu1 = notifyMenu
+    var personalMenu1 = personalMenu
+    var settingMenu1 = settingMenu
+    TopAppBar(
+        title = {
+            Text(text = "Solvo")
+        },
+        navigationIcon = {
+            IconButton(onClick = {/* Go to home page */ }) {
+                Icon(Icons.Filled.Home, null)
+            }
+        },
+        actions = {
+            IconButton(onClick = { notifyMenu1 = true }) {
+                Icon(Icons.Filled.Notifications, null)
+            }
+            IconButton(onClick = { personalMenu1 = true }) {
+                Icon(Icons.Filled.Person, null)
+            }
+            IconButton(onClick = { settingMenu1 = true }) {
+                Icon(Icons.Filled.Settings, null)
+            }
+            DropdownMenu(
+                expanded = notifyMenu1,
+                onDismissRequest = { notifyMenu1 = false },
+                offset = DpOffset(x = 10.dp, y = (-60).dp)
+            ) {
+                DropdownMenuItem(
+                    text = {},
+                    onClick = {},
+                )
+            }
 //                    DropdownMenu(
 //                        expanded = personalMenu,
 //                        onDismissRequest = { personalMenu = false},
@@ -81,11 +108,35 @@ fun HomePageContent() {
 //                    ) {
 //
 //                    }
-                },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer
-                )
-            )
+        },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        )
+    )
+}
+
+@Composable
+private fun createCourses(courseNames: List<String>) {
+    FlowRow(
+        modifier = Modifier.padding(20.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        for (courseName in courseNames) {
+            createButtonForCourse(courseName)
         }
+    }
+}
+
+@Composable
+private fun createButtonForCourse(courseName: String) {
+    ElevatedCard(
+        onClick = {},
+        modifier = Modifier.padding(20.dp).height(160.dp).width(200.dp).clickable {},
+        shape = RoundedCornerShape(8.dp),
+    ) {
+        Text(
+            text = courseName,
+            fontSize = MaterialTheme.typography.headlineSmall.fontSize
+        )
     }
 }
