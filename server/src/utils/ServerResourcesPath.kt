650a1316ca06cacc2809f1b210513fde82e9246e
Jerry Zhu <yuqian.zhu21@imperial.ac.uk>
2023-06-01 18:50:52 +0100

    add server/test-sandbox/test-resources to gitignore exception

--- server/src/utils/ServerResourcesPath.kt
+++ server/src/utils/ServerResourcesPath.kt
@@ -1,20 +1,27 @@
 package org.solvo.server.utils
 
+import java.nio.file.Path
 import java.util.*
 
 interface ServerResourcesPath {
-    fun databasePath(): String
-    fun staticResourcePath(resourceId: UUID, purpose: StaticResourcePurpose): String
+    fun databasePath(): Path
+    fun staticResourcePath(resourceId: UUID, purpose: StaticResourcePurpose): Path
 }
 
 class ServerResourcesPathImpl : ServerResourcesPath {
-    private val host: String = System.getProperty("user.dir")
+    private val remote: String = "http://localhost"
+    private val local: String = System.getProperty("user.dir")
 
-    override fun databasePath(): String {
-        return "$host/db"
+    override fun databasePath(): Path {
+        return Path.of("$local/db")
     }
 
-    override fun staticResourcePath(resourceId: UUID, purpose: StaticResourcePurpose): String {
-        return "$host/resources/$purpose/$resourceId"
+    override fun staticResourcePath(resourceId: UUID, purpose: StaticResourcePurpose): Path {
+        return Path.of("$local/resources/$purpose/$resourceId")
     }
 }
+
+class ResourcePaths(
+    localPath: Path,
+    remotePath: Path,
+)
