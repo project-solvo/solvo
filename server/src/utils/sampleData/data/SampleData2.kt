fc95db66f6363c7eb6689f808d2df66bb4a39fa6
Him188 <Him188@mamoe.net>
2023-06-15 18:05:38 +0100

    Update sampleData2

--- server/src/utils/sampleData/data/SampleData2.kt
+++ server/src/utils/sampleData/data/SampleData2.kt
@@ -1,22 +1,19 @@
 package org.solvo.server.utils.sampleData.data
 
-import io.ktor.http.*
 import org.solvo.server.modules.AuthDigest
 import org.solvo.server.utils.sampleData.builder.SampleDataBuilder
 
 fun SampleDataBuilder.sampleData2() {
-    val xi = user("Xi", AuthDigest("china")) {
-        avatar("./test-resources/secret_avatar", ContentType.Image.JPEG)
-    }
+    val charles = user("charles", AuthDigest("charles")) {}
     val questionsList = listOf("1a", "1b", "1c", "1d", "2a", "2b", "2c")
     course("50009", "Symbolic Reasoning") {
-        article("Paper_2022", xi) {
+        article("Paper_2022", charles) {
             defaultQuestions(questionsList)
             anonymous()
             question("1e") {
                 content { "## Question blah blah blah..!" }
                 anonymous()
-                answer(xi) {
+                answer(charles) {
                     content("Haha, you found me!")
                 }
             }
